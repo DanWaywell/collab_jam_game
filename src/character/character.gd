cf44e8cfc83bb55c830ac8431bd281ac4628792e
DanWaywell <34028193+DanWaywell@users.noreply.github.com>
2025-04-05 14:22:27 +0100

    add functions
    functions for setting direction facing, sprite checking health and taking damage

--- src/character/character.gd
+++ src/character/character.gd
@@ -6,6 +6,9 @@ const DECELERATION = 20.0
 
 const PROJECTILE = preload("res://projectile/projectile.tscn")
 
+var direction_facing = Vector2.RIGHT
+
+var health = 3
 
 func _physics_process(delta: float) -> void:
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -14,19 +17,63 @@ func _physics_process(delta: float) -> void:
 
 	# Movement x y
 	if move_direction:
-		velocity.x = move_toward(velocity.x, move_direction.x * SPEED * input_direction.length(), ACCELERATION)
-		velocity.y = move_toward(velocity.y, move_direction.y * SPEED * input_direction.length(), ACCELERATION)
+		velocity = move_direction * SPEED
+		#velocity.x = move_toward(velocity.x, move_direction.x * SPEED * input_direction.length(), ACCELERATION)
+		#velocity.y = move_toward(velocity.y, move_direction.y * SPEED * input_direction.length(), ACCELERATION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, DECELERATION)
-		velocity.y = move_toward(velocity.y, 0, DECELERATION)
+		velocity = Vector2.ZERO
+		#velocity.x = move_toward(velocity.x, 0, DECELERATION)
+		#velocity.y = move_toward(velocity.y, 0, DECELERATION)
 
 	move_and_slide()
 	
+	set_cardinal_direction(input_direction)
+	set_sprite()
+	check_health()
 	
 	if Input.is_action_just_pressed("action_1"):
 		fire_projectile()
-
+	
+	
 func fire_projectile():
 	var new_projectile = PROJECTILE.instantiate()
 	new_projectile.position = position
+	new_projectile.direction = direction_facing
 	add_sibling(new_projectile)
+
+
+func set_cardinal_direction(vec2):
+	if vec2 != Vector2.ZERO:
+		if abs(vec2.x) > abs(vec2.y):
+			if vec2.x > 0:
+				direction_facing = Vector2.RIGHT
+			else:
+				direction_facing = Vector2.LEFT
+		else:
+			if vec2.y > 0:
+				direction_facing = Vector2.DOWN
+			else:
+				direction_facing = Vector2.UP
+
+
+func set_sprite():
+	var sprite = $Sprite2D
+	if direction_facing == Vector2.UP:
+		sprite.frame = 0
+	elif direction_facing == Vector2.RIGHT:
+		sprite.frame = 1
+	elif direction_facing == Vector2.DOWN:
+		sprite.frame = 2
+	elif direction_facing == Vector2.LEFT:
+		sprite.frame = 3
+	else:
+		pass
+
+
+func take_damage(damage):
+	health -= damage
+
+
+func check_health():
+	if health < 1:
+		queue_free()
