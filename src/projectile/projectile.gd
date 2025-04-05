b2eb6ec02ba6af01a0e84ece1db3c8eddd087199
DanWaywell <34028193+DanWaywell@users.noreply.github.com>
2025-04-05 14:23:17 +0100

    add damage and make sprite rotate to direction

--- src/projectile/projectile.gd
+++ src/projectile/projectile.gd
@@ -3,7 +3,17 @@ extends Area2D
 
 var speed = 80.0
 var direction = Vector2.RIGHT
+var damage = 1
+
+
+func _ready() -> void:
+	look_at(position + direction)
 
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
