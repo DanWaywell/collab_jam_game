2937ee3d86d87e4fcea11c110cd142d73f46229c
DanWaywell <34028193+DanWaywell@users.noreply.github.com>
2025-04-14 11:23:48 +0100

    dev_3 manual merge

--- src/player/process_effects.gd
+++ src/player/process_effects.gd
@@ -4,9 +4,11 @@ extends Node
 # processes effects that need to be in a process function
 
 
-#func _process(_delta: float) -> void:
-	#for potion in potions.effects:
-		#potion.effects.positive_effect.effect_process()
-		#potion.effects.negative_effect.effect_process()
-		#pass
-	#pass
+func _process(_delta: float) -> void:
+	#positive process effects
+	if potions.pos_effect_pool.size() > 0:
+		for potion in potions.effects:
+			potion.effects.positive_effect.effect_process()
+			potion.effects.negative_effect.effect_process()
+			pass
+		pass
